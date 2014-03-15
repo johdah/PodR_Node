3f29376c9add39e60c39e13c4923591b77e648d3
Johan Dahlberg <johan@dahlberg.co>
2014-03-15 20:30:26 +0100

    Adding UserSchema Methods

--- app/models/user.js
+++ app/models/user.js
@@ -29,3 +29,56 @@ var UserSchema = new Schema({
 	linkedin: {},
 	twitter: {}
 });
+
+/**
+ * Virtuals
+ */
+
+/**
+ * Validations
+ */
+
+/**
+ * Pre-save hook
+ */
+
+/**
+ * Methods
+ */
+UserSchema.Methods = {
+	/**
+	 * Authenticate - check if the passwords are the same
+	 *
+  	 * @param {String} plainText
+  	 * @return {Boolean}
+  	 * @api public
+	 */
+	authenticate: function(plainText) {
+		return this.encryptPassword(plainText) === this.hashed_password;
+	},
+
+	/**
+	 * Make salt
+	 *
+	 * @return {String}
+	 * @api public
+	 */
+	makeSalt: function() {
+		return crypto.randomBytes(16).toString('base64');
+	},
+
+	/**
+	 * Encrypt password
+	 *
+	 * @param {String} password
+	 * @return {String}
+	 * @api public
+	 */
+	encryptPassword: function(password) {
+		if(!password || !this.salt) return '';
+		var salt = new Buffer(this.salt, 'base64');
+		return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+	}
+};
+
+mongoose.model('User', UserSchema);
