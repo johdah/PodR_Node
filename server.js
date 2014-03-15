10c30bbf8a5859eecfb4242e2f2aba63e350dc4e
Johan Dahlberg <johan@dahlberg.co>
2014-03-15 19:38:26 +0100

    Bootstraping models in server.js

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ var config = require('./config/config'),
 var db = mongoose.connect(config.db);
 
 // Bootstrap models
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+	fs.readdirSync(path).forEach(function(file) {
+		var newPath = path + '/' + file;
+		var stat = fs.statSync(newPath);
+		if (stat.isFile()) {
+			if(/(.*)\.(js$|coffee$)/.test(file)) {
+				require(newPath);
+			}
+		} else if (stat.isDirectory()) {
+			walk(newPath);
+		}
+	});
+};
+walk(models_path);
 
 // Bootstrap passport config
 
