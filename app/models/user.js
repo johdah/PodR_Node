e685bcdd466b7fd92022c9a0853d254a5b997b4b
Johan Dahlberg <johan@dahlberg.co>
2014-03-15 20:40:08 +0100

    Adding validation and virtuals to the UserSchema

--- app/models/user.js
+++ app/models/user.js
@@ -33,14 +33,54 @@ var UserSchema = new Schema({
 /**
  * Virtuals
  */
+UserSchema.virtual('password').set(function(password) {
+ 	this._password = password;
+ 	this.salt = this.makeSalt();
+ 	this.hashed_password = this.encryptPassword(password);
+}).get(function() {
+ 	return this._password;
+});
 
 /**
  * Validations
  */
+var validatePresenceOf = function(value) {
+	return value && value.length;
+};
+
+// The 4 validations below only apply if you are signing up traditionally
+UserSchema.path('name').validate(function(name) {
+	// If you are authenticating by any of the oauth strategies, don't validate.
+	if(!this.provider) return true;
+	return (typeof name === 'string' && name.length > 0);
+}, 'Name cannot be blank');
+UserSchema.path('email').validate(function(email) {
+	// If you are authenticating by any of the oauth strategies, don't validate.
+	if(!this.provider) return true;
+	return (typeof email === 'string' && email.length > 0);
+}, 'Email cannot be blank');
+UserSchema.path('username').validate(function(username) {
+	// If you are authenticating by any of the oauth strategies, don't validate.
+	if(!this.provider) return true;
+	return (typeof username === 'string' && username.length > 0);
+}, 'Username cannot be blank');
+UserSchema.path('hashed_password').validate(function(hashed_password) {
+	// If you are authenticating by any of the oauth strategies, don't validate.
+	if(!this.provider) return true;
+	return (typeof hashed_password === 'string' && hashed_password.length > 0);
+}, 'Password cannot be blank');
 
 /**
  * Pre-save hook
  */
+UserSchema.pre('save', function(next) {
+	if(!this.isNew) return next();
+
+	if(!validatePresenceOf(this.password) && !this.provider)
+		next(new Error('Invalid password'));
+	else
+		next();
+});
 
 /**
  * Methods
