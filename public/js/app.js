4aa27e69b9b2e8c8a538674edae335fe0a9a48e7
Johan Dahlberg <johan@dahlberg.co>
2014-03-16 01:38:29 +0100

    Adding app.js

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('podr', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'podr.system', 'podr.podcasts']);
+
+angular.module('podr.system', []);
+angular.module('podr.podcasts', []);
