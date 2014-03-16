4cb6dd3063a7b579ba3cd788cb9df7e76ac3cd9f
Johan Dahlberg <johan@dahlberg.co>
2014-03-16 02:14:46 +0100

    Updata gruntfile.js, package.json and adding jshint

--- gruntfile.js
+++ gruntfile.js
@@ -3,9 +3,50 @@
 module.exports = function(grunt) {
 	// Project Configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			js: {
+				files: ['gruntfile.js', 'server.js', 'app/**/*.js', 'public/js/**', 'test/**/*.js'],
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			html: {
+				files: ['public/views/**', 'app/views/**'],
+				options: {
+					livereload: true
+				}
+			},
+			css: {
+				files: ['public/css/**'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		jshint: {
+			all: {
+				src: ['gruntfile.js', 'server.js', 'app/**/*.js', 'public/js/**', 'test/**/*.js', '!test/coverage/**/*.js'],
+				options: {
+					jshintrc: true
+				}
+			}
+		},
+		env: {
+			test: {
+				NODE_ENV: 'test'
+			}
+		}
 	});
 
-	// Making grunt default to force in order not to break the projects.
+	// Load NPM tasks
+
+	// Making grunt default to force in order not to break the project.
 	grunt.option('force', true);
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'concurrent']);
+
+	// Test task.
 };
