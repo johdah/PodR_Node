4c38c9cd248a02e899a1a2d812bda644635844c1
Johan Dahlberg <johan@dahlberg.co>
2014-03-16 01:26:59 +0100

    Add the index controller

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,4 +5,21 @@ var users = require('../controllers/users');
 
 module.exports = function(app, passport) {
 	
+	app.get('/signin', users.signin);
+	app.get('/signup', users.signup);
+	app.get('/signout', users.signout);
+	app.get('/users/me', users.me);
+
+	// Setting up the users api
+	app.post('/users', users.create);
+
+	// Setting up the userId param
+	app.param('userId', users.user);
+
+	// Setting the local strategy route
+	app.post('/users/session', passport.authenticate('local', {
+		failureRedirect: '/signin',
+		failureFlash: true
+	}), users.session);
+
 };
