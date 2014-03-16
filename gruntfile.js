3623e0fbdc837c8aad0b1e706a07942988bb15bb
Johan Dahlberg <johan@dahlberg.co>
2014-03-16 15:26:37 +0100

    Adding mocha testing

--- gruntfile.js
+++ gruntfile.js
@@ -55,6 +55,13 @@ module.exports = function(grunt) {
 				logConcurrentOutput: true
 			}
 		},
+		mochaTest: {
+			options: {
+				reporter: 'spec',
+				require: 'server.js'
+			},
+			src: ['test/mocha/**/*.js']
+		},
 		env: {
 			test: {
 				NODE_ENV: 'test'
@@ -65,6 +72,7 @@ module.exports = function(grunt) {
 	// Load NPM tasks
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-nodemon');
 	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-env');
@@ -76,4 +84,5 @@ module.exports = function(grunt) {
 	grunt.registerTask('default', ['jshint', 'concurrent']);
 
 	// Test task.
+	grunt.registerTask('test', ['env:test', 'mochaTest']);
 };
