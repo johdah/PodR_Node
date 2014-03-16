fb2409eee8bc53358e7ca2fd98c0d0f5a847ca00
Johan Dahlberg <johan@dahlberg.co>
2014-03-16 02:00:26 +0100

    Adding init.js to the frontend

---
+++ public/js/init.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.element(document).ready(function() {
+	// Fixing facebook bug with redirect
+	if (window.locaiton.hash === '#_=_') 
+		window.locaiton.hash = '#!';
+
+	// Then init the app
+	angular.bootstrap(document, ['podr']);
+});
